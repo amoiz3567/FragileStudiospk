371e524b81b7867d5e0f97aa4f26022ee9f8b173
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-25 10:33:57 +0000

    meow

--- root/static/js/header.js
+++ root/static/js/header.js
@@ -9,7 +9,8 @@ document.addEventListener('scroll', (event) => {
             header.classList.add("frao");
             header.style.transform = "translate(0, 0%)";
             fk.style.display = "block";
-            frg.style.margin = "-20px";
+            frg.style.margin = "0px";
+            frg.style.margintOP = "-20px";
             frg.style.width = "140px";
             //setTimeout(() => header.style.transition ="0s", 50);
         }, 1);
