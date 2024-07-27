31692d65b60a49c5912a9b8e713b26b038eface4
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-27 15:55:33 -0700

    master

--- root/static/js/header.js
+++ root/static/js/header.js
@@ -10,10 +10,10 @@ document.addEventListener('scroll', (event) => {
             header.style.transform = "translate(0, 0%)";
             fk.style.display = "block";
             frg.style.margin = "0px";
-            //frg.style.marginTop = "-20px";
-            frg.style.marginBottom = "20px";
+            frg.style.marginTop = "-20px";
+            //frg.style.marginBottom = "20px";
             frg.style.width = "140px";
-            frg.style.width = "10px";
+            //frg.style.width = "10px";
             //setTimeout(() => header.style.transition ="0s", 50);
         }, 1);
 
