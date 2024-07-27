0195ebeb29b166a1de8d6d9997ccf8fceb2871a7
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-27 03:45:39 -0700

    master

--- main.py
+++ main.py
@@ -1072,7 +1072,7 @@ def checkout_cart():
                 crate_len = len(a)
         price = 0
         for i in a:
-            i = json.loads(i)
+            i = json.loads(i.replace("\'", "\""))
             price += int(i['price'])+200
         bruv = make_response(render_template('checkout.html', a="cart", total=price, amount=crate_len))
         token = generate_token(uuid.uuid4())
