b6e578a355f94c00c519151556a52d91fe960de7
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-28 06:27:33 -0700

    master

--- main.py
+++ main.py
@@ -952,7 +952,7 @@ def retProduct(p=None, raf=None):
     cursorCategory = ""
     if category_id != None and category_id != "" and not(Invaliduuid12(category_id)):
         print(category_id, " - - - - -- - -")
-        cursorCategory = f" WHERE INSTR(category_also, '{category_id}') > 0"
+        cursorCategory = f" WHERE INSTR(belongs, '{category_id}') > 0"
     if (l_ != "" and cursorCategory != ""):
         l_ = " AND price BETWEEN %s AND %s"
     id = request.cookies.get("id")
@@ -1337,7 +1337,7 @@ def request_pr_read(data, nom):
             FROM 
                 products
             WHERE 
-                INSTR(category_also, %s) > 0
+                INSTR(belongs, %s) > 0
             ORDER BY 
                 main_category, price
         ) AS subquery
