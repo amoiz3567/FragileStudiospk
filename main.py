c5c4394075363e7a8a829207126507da68ba1c0d
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-28 14:55:15 -0700

    d

--- main.py
+++ main.py
@@ -899,7 +899,7 @@ def red_(data):
         if (json.loads(car_json[v].replace("\'", "\""))['productId']) == data['0']:
             b = (f"\"{v}\": \"{car_json[v]}\"")
         a += 1
-    print(b, "hehehehehe")
+    print(b, " hehehehehe ", data['0'])
     print(repair_json(car.replace(b, '')))
     res = json.loads(repair_json(car.replace(b, '')))
     #cursor.execute("UPDATE users SET cart = %s WHERE user_id = %s;", (json.dumps(res), id))
