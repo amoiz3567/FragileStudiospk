8802ff34812180b57cd16032d0240043b3298294
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-28 23:03:17 -0700

    mas

--- root/static/js/header.js
+++ root/static/js/header.js
@@ -23,7 +23,7 @@ document.addEventListener('scroll', (event) => {
         fk.style.display = "none";
         header.style.opacity = "1";
         frg.style.margin = "0";
-        frg.style.marginBottom = "15px";
+        //frg.style.marginBottom = "15px";
         frg.style.marginTop = "20px";
         frg.style.width = ""; //240px
     }
