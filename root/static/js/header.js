a05f265f71e0c9dac0b2130a9d60eb1b99336a12
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-29 04:07:22 -0700

    a

--- root/static/js/header.js
+++ root/static/js/header.js
@@ -12,8 +12,10 @@ document.addEventListener('scroll', (event) => {
             frg.style.margin = "0px";
             frg.style.marginTop = "";
             //frg.style.marginBottom = "20px";
-            if (window.innerWidth <= 475)
-            frg.style.marginBottom = "0";
+            if (window.innerWidth <= 475) {
+                console.log("yes this works now yay!");
+                frg.style.marginBottom = "0";
+            }
             frg.style.width = "140px";
             //frg.style.width = "10px";
             //setTimeout(() => header.style.transition ="0s", 50);
@@ -28,8 +30,10 @@ document.addEventListener('scroll', (event) => {
         fk.style.display = "none";
         header.style.opacity = "1";
         frg.style.margin = "0";
-        if (window.innerWidth <= 475)
-        frg.style.marginBottom = "15px";
+        if (window.innerWidth <= 475) {
+            console.log("yes this works now yay!");
+            frg.style.marginBottom = "15px";
+        }
         frg.style.marginTop = "20px";
         frg.style.width = ""; //240px
     }
