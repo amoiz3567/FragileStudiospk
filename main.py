06fbb6fb369a941dc711bb9f64021463308e5b23
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-30 03:23:26 -0700

    ad

--- main.py
+++ main.py
@@ -897,6 +897,7 @@ def red_(data, f="0"):
     #cursor.execute("SELECT cart FROM users WHERE user_id = %s;", (str(id),))
     #car = cursor.fetchall()
     if (f == "1"):
+        print(data[0])
         data = jwt.decode(data[0], app.secret_key, algorithms=['HS256'])[0]['csrf']
         print(data, "\n\n\n the decoded data\n")
     car = session["yourCart"]
