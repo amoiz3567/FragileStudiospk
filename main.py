183b8494837ac1dbe4cdd312dc9af6b5902bca4a
valdmir <70150709+amoiz3567@users.noreply.github.com>
2024-07-30 04:17:14 -0700

    awd

--- main.py
+++ main.py
@@ -893,7 +893,7 @@ def save(productid):
 @socketio.on("rmc")
 @before_mid
 def red_(data__, f="0"):
-    data = data__[0]
+    data = data__
     try:
         print("removing "+str(data__['0']))
         data = data__['0']
